5eea971d16911a0da51cce21a8f3d5ad4c738020
RuslanFenomen <77931033+RuslanFenomen@users.noreply.github.com>
2025-02-11 15:04:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ const swiper = new Swiper(".price__swiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+// popup
+const popupBg = document.querySelector('.popup__bg');
+const popup = document.querySelector('.popup');
+const openPopupButtons = document.querySelectorAll('.open-popup');
+const closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
